817b9102ef7b58e0d45a0340e265fca2bd49153c
Chad Whitacre <chad@zetaweb.com>
2012-06-13 22:10:51 -0400

    Stashing early work on Facebook login
    I started hacking at Facebook authentication, but decided that there
    were some bugs to fix before we go down that route. I think once we
    add Facebook and to a lesser extent Twitter and Google sign-in, we're
    going to see another order of magnitude growth (assuming we get there
    before losing momentum). I'd like to take an extra week and fix up some
    bugs before that. Hopefully I'm not shooting us in the foot!

--- configure-aspen.py
+++ configure-aspen.py
@@ -13,9 +13,20 @@ website.github_client_id = os.environ['GITHUB_CLIENT_ID']
 website.github_client_secret = os.environ['GITHUB_CLIENT_SECRET']
 website.github_callback = os.environ['GITHUB_CALLBACK']
 
+website.facebook_client_id = os.environ['FACEBOOK_CLIENT_ID']
+website.facebook_client_secret = os.environ['FACEBOOK_CLIENT_SECRET']
+website.facebook_callback = os.environ['FACEBOOK_CALLBACK']
+
 website.hooks.inbound_early.register(gittip.canonize) 
 website.hooks.inbound_early.register(gittip.authentication.inbound) 
 website.hooks.outbound_late.register(gittip.authentication.outbound) 
+
+def facebook_oauth_url(then=""):
+    url = "https://www.facebook.com/dialog/oauth?client_id=%s"
+    url %= website.facebook_client_id
+    if url:
+        url += "&redirect_uri=%s?then=%s" % (website.facebook_callback, then)
+    return url
 
 def github_oauth_url(then=""):
     url = "https://github.com/login/oauth/authorize?client_id=%s" 
@@ -27,6 +38,7 @@ def github_oauth_url(then=""):
 def add_stuff(request):
     request.context['__version__'] = gittip.__version__
     request.context['username'] = None 
+    request.context['facebook_oauth_url'] = facebook_oauth_url
     request.context['github_oauth_url'] = github_oauth_url
 
 website.hooks.inbound_early.register(add_stuff) 
